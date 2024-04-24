33c83b16d077041e2fc2c354fdba2d27d75e398c
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 <scohail206@gmail.com>
2024-04-24 16:49:45 +0100

    structered code

--- Under_Sampling_Techniques.py
+++ Under_Sampling_Techniques.py
@@ -7,7 +7,7 @@ from imblearn.under_sampling import NearMiss
 import pandas as pd
 from collections import Counter
 
-def Random_Undersampling(data):
+def Random_Undersampling_tec(data):
     rus = RandomUnderSampler(sampling_strategy='auto', random_state=0)
     X_resampled_RandUnder, y_resampled_RandUnder = rus.fit_resample(data.drop('Class', axis=1), data['Class'])
     print('Original dataset shape %s' % Counter(data['Class']))
@@ -16,8 +16,8 @@ def Random_Undersampling(data):
 
     return data_resampled
 
-def Tomek_Links(data):
-    tl = TomekLinks(sampling_strategy='auto')
+def TomekLinks_Tec(data):
+    tl = TomekLinks(sampling_strategy='auto',)
     X_resampled_tomek, y_resampled_tomek = tl.fit_resample(data.drop('Class', axis=1), data['Class'])
     print('Original dataset shape %s' % Counter(data['Class']))
     print('Resampled dataset shape %s' % Counter(y_resampled_tomek))
@@ -25,7 +25,7 @@ def Tomek_Links(data):
 
     return data_resampled
 
-def ENN(data):
+def ENN_Tec(data):
     enn = EditedNearestNeighbours(sampling_strategy='auto', n_neighbors=31, kind_sel='all')
     X_resampled_enn, y_resampled_enn = enn.fit_resample(data.drop('Class', axis=1), data['Class'])
     print('Original dataset shape %s' % Counter(data['Class']))
@@ -34,7 +34,7 @@ def ENN(data):
 
     return data_resampled
 
-def NearMiss1(data):
+def NearMiss1_Tec(data):
     nm1 = NearMiss(
             sampling_strategy='auto',  # undersamples only the majority class
             version=1,
@@ -47,7 +47,7 @@ def NearMiss1(data):
 
     return data_resampled
 
-def NearMiss2(data):
+def NearMiss2_Tec(data):
     nm1 = NearMiss(
             sampling_strategy='auto',  # undersamples only the majority class
             version=2,
@@ -59,7 +59,7 @@ def NearMiss2(data):
     data_resampled = pd.concat([X_resampled_NearMiss2, y_resampled_NearMiss2], axis=1)
 
     return data_resampled
-def NearMiss3(data):
+def NearMiss3_Tec(data):
     nm1 = NearMiss(
             sampling_strategy='auto',  # undersamples only the majority class
             version=3,
